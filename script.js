0bcb1ee4dae3aa02096fb5499a43757b0f8ff203
Durga Prasad Bejawada <117895509+Durga0808@users.noreply.github.com>
2023-12-15 17:58:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,20 @@ const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
   // Write your code here
+	const ans=[];
+	let i=0,j=0,sum=0;
+
+	while(j<arr.length){
+		sum+=arr[j];
+		if(sum>n){
+			ans.push(arr.slice(i,j));
+			sum=arr[j];
+			i=j;
+		}
+		j++;
+	}
+	ans.push(arr.slice(i,j));
+	return ans;
 };
 
 const n = prompt("Enter n: ");
